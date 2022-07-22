25917dce68976fa8461e5143f50e562ad6e5aa6a
edric <edricgomes@gmail.com>
2022-07-22 09:14:42 +0100

    Initial commit

---
+++ cap03/exercise1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println("ùnica declaração:", x, y, z)
+
+	fmt.Println("Múltiplas declarações")
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
